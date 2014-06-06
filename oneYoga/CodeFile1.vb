268b844b639fe2a384467f4796907f080f1134ee
ellMathur <lpbarce@gmail.com>
2014-06-06 16:29:36 +0800

    initial commit
    initial commit containing draft members database table, and login form

---
+++ oneYoga/CodeFile1.vb
@@ -0,0 +1,106 @@
+﻿
+Imports System.Security.Cryptography
+Public NotInheritable Class Simple3Des
+
+    Private TripleDes As New TripleDESCryptoServiceProvider
+
+    Private Function TruncateHash(
+    ByVal key As String,
+    ByVal length As Integer) As Byte()
+
+        Dim sha1 As New SHA1CryptoServiceProvider
+
+        ' Hash the key. 
+        Dim keyBytes() As Byte =
+            System.Text.Encoding.Unicode.GetBytes(key)
+        Dim hash() As Byte = sha1.ComputeHash(keyBytes)
+
+        ' Truncate or pad the hash. 
+        ReDim Preserve hash(length - 1)
+        Return hash
+    End Function
+
+    Sub New(ByVal key As String)
+        ' Initialize the crypto provider.
+        TripleDes.Key = TruncateHash(key, TripleDes.KeySize \ 8)
+        TripleDes.IV = TruncateHash("", TripleDes.BlockSize \ 8)
+    End Sub
+
+    Public Function EncryptData(
+    ByVal plaintext As String) As String
+
+        ' Convert the plaintext string to a byte array. 
+        Dim plaintextBytes() As Byte =
+            System.Text.Encoding.Unicode.GetBytes(plaintext)
+
+        ' Create the stream. 
+        Dim ms As New System.IO.MemoryStream
+        ' Create the encoder to write to the stream. 
+        Dim encStream As New CryptoStream(ms,
+            TripleDes.CreateEncryptor(),
+            System.Security.Cryptography.CryptoStreamMode.Write)
+
+        ' Use the crypto stream to write the byte array to the stream.
+        encStream.Write(plaintextBytes, 0, plaintextBytes.Length)
+        encStream.FlushFinalBlock()
+
+        ' Convert the encrypted stream to a printable string. 
+        Return Convert.ToBase64String(ms.ToArray)
+    End Function
+
+    Public Function DecryptData(
+    ByVal encryptedtext As String) As String
+
+        ' Convert the encrypted text string to a byte array. 
+        Dim encryptedBytes() As Byte = Convert.FromBase64String(encryptedtext)
+
+        ' Create the stream. 
+        Dim ms As New System.IO.MemoryStream
+        ' Create the decoder to write to the stream. 
+        Dim decStream As New CryptoStream(ms,
+            TripleDes.CreateDecryptor(),
+            System.Security.Cryptography.CryptoStreamMode.Write)
+
+        ' Use the crypto stream to write the byte array to the stream.
+        decStream.Write(encryptedBytes, 0, encryptedBytes.Length)
+        decStream.FlushFinalBlock()
+
+        ' Convert the plaintext stream to a string. 
+        Return System.Text.Encoding.Unicode.GetString(ms.ToArray)
+    End Function
+
+End Class
+
+
+Public Class samp
+
+    Sub TestEncoding()
+        Dim plainText As String = InputBox("Enter the plain text:")
+        Dim password As String = InputBox("Enter the password:")
+
+        Dim wrapper As New Simple3Des(password)
+        Dim cipherText As String = wrapper.EncryptData(plainText)
+
+        MsgBox("The cipher text is: " & cipherText)
+        My.Computer.FileSystem.WriteAllText(
+            My.Computer.FileSystem.SpecialDirectories.MyDocuments &
+            "\cipherText.txt", cipherText, False)
+    End Sub
+
+    Sub TestDecoding()
+        Dim cipherText As String = My.Computer.FileSystem.ReadAllText(
+            My.Computer.FileSystem.SpecialDirectories.MyDocuments &
+                "\cipherText.txt")
+        Dim password As String = InputBox("Enter the password:")
+        Dim wrapper As New Simple3Des(password)
+
+        ' DecryptData throws if the wrong password is used. 
+        Try
+            Dim plainText As String = wrapper.DecryptData(cipherText)
+            MsgBox("The plain text is: " & plainText)
+        Catch ex As System.Security.Cryptography.CryptographicException
+            MsgBox("The data could not be decrypted with the password.")
+        End Try
+    End Sub
+
+End Class
