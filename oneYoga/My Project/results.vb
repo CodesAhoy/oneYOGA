fdeb14466e78228231828e2d17d1f0033edec353
ellMathur <lpbarce@gmail.com>
2014-06-07 20:29:50 +0800

    working members registration function
    database for members' profile lack number 5 item in health issues. it is
    needed to be in another table - not under member table.

--- oneYoga/My Project/results.vb
+++ oneYoga/My Project/results.vb
@@ -1,27 +1,38 @@
 ﻿Public Class results
 
   
-    Private Sub MembersBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles MembersBindingNavigatorSaveItem.Click
-        Me.Validate()
-        Me.MembersBindingSource.EndEdit()
-        Me.TableAdapterManager.UpdateAll(Me.Database1DataSet)
 
-    End Sub
 
     Private Sub results_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
         loginForm.Show()
 
     End Sub
 
-    Private Sub results_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'Database1DataSet.members' table. You can move, or remove it, as needed.
-        Me.MembersTableAdapter.Fill(Me.Database1DataSet.members)
+
+
+
+
+
+    Private Sub results_Load(sender As Object, e As EventArgs)
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub MembersBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles MembersBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.MembersBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.OYmembers)
+
+    End Sub
+
+    Private Sub results_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'OYmembers.members' table. You can move, or remove it, as needed.
+        Me.MembersTableAdapter.Fill(Me.OYmembers.members)
+
+    End Sub
+
+ 
+    Private Sub Close_button_Click(sender As Object, e As EventArgs) Handles Close_button.Click
         loginForm.Show()
         Me.Close()
-
     End Sub
 End Class
